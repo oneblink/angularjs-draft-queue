0d6185f3d3e331b617a0fe2595bc77a27912a8a5
simonmarklar <simondobie23@gmail.com>
2017-06-19 16:06:35 +1000

    add protractor e2e tests

--- test/e2e/index.js
+++ test/e2e/index.js
@@ -11,9 +11,13 @@ app.controller('testCtrl', ['$http', '$scope', '$timeout', '$log', 'draftQueue',
     const $ctrl = this
     $ctrl.model = {}
 
-    $ctrl.saveDraft = function(item) {
-      draftQueue.setItem(item, 'testCtrl').then((item) => {
+    $ctrl.saveDraft = function(model) {
+      draftQueue.setItem(model, 'testCtrl').then((item) => {
         $ctrl.lastSaved = item.model._uuid
+        item.dateCreated = new Date(item.dateCreated)
+        item.dateModified = new Date(item.dateModified)
+        $ctrl.draftItem = item
+        $ctrl.model = angular.copy(item.model)
       })
     }
   }
--- test/protractor/spec.js
+++ test/protractor/spec.js
@@ -1,20 +1,30 @@
 'use strict'
 
-function selectDropdownByNumber(element, index, milliseconds) {
-    element.all(by.tagName('option'))
-      .then(function(options) {
-        options[index].click();
-      });
-    if (typeof milliseconds !== 'undefined') {
-      browser.sleep(milliseconds);
-   }
+const EC = protractor.ExpectedConditions
+
+function selectDropdownByNumber(element, index) {
+  return element.all(by.tagName('option'))
+    .then(function(options) {
+      options[index].click();
+    })
 }
 
 describe('Protractor tests', () => {
+  const expectedFormName = 'testCtrl'
+  const expectedText = 'expected text'
+  const expectedSelectValue = 'Select 1'
+
   let textbox
   let selectbox
   let saveDraftButton
   let lastSaved
+
+  // update elements
+  let updatedTextbox
+  let updatedSelectbox
+  let updatedDateCreated
+  let updatedDateModified
+  let updatedFormName
 
   beforeEach(() => {
     browser.get('http://localhost:8000/test/e2e/index.html')
@@ -22,6 +32,12 @@ describe('Protractor tests', () => {
     textbox = element(by.model('$ctrl.model.text'))
     selectbox = element(by.model('$ctrl.model.select'))
     saveDraftButton = element(by.id('save-draft'))
+
+    updatedTextbox = element(by.model('$ctrl.draftItem.model.text'))
+    updatedSelectbox = element(by.model('$ctrl.draftItem.model.select'))
+    updatedDateCreated = element(by.model('$ctrl.draftItem.dateCreated'))
+    updatedDateModified = element(by.model('$ctrl.draftItem.dateModified'))
+    updatedFormName = element(by.model('$ctrl.draftItem.form'))
 
     lastSaved = element(by.id('last-saved'))
   })
@@ -31,18 +47,65 @@ describe('Protractor tests', () => {
     expect(selectbox.isPresent()).toBe(true)
   })
 
-  it('should save the model', () => {
+  it('should save the model', (done) => {
     const expectedText = 'expected text'
     textbox.sendKeys(expectedText)
-    selectDropdownByNumber(selectbox, 0)
+    selectDropdownByNumber(selectbox, 1)
+      .then(() => browser.waitForAngular())
+      .then(() => saveDraftButton.click())
+      .then(() => browser.waitForAngular())
+      .then(() => {
+        const options = updatedSelectbox.all(by.tagName('option'))
 
-    browser.waitForAngular().then(() => {
-      saveDraftButton.click()
+        expect(lastSaved.getText()).not.toBeUndefined()
+        expect(lastSaved.getText()).toEqual(jasmine.stringMatching(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/))
+        EC.textToBePresentInElementValue(updatedTextbox, expectedText)
+        expect(options.count()).toBe(2)
+        expect(options.first().isSelected()).toBe(true)
+        expect(options.first().getText()).toBe(expectedSelectValue)
+        expect(updatedDateCreated).not.toBeUndefined()
+        EC.textToBePresentInElementValue(updatedFormName, expectedFormName)
+      })
+      .then(() => updatedDateCreated.getAttribute('value').then((created) => {
+        return updatedDateModified.getAttribute('value').then((updated) => expect(updated).toBe(created))
+      }))
+      .then(done)
+  })
 
-      return browser.waitForAngular()
-    }).then(() => {
-      expect(lastSaved.getText()).not.toBeUndefined()
-      expect(lastSaved.getText()).toEqual(jasmine.stringMatching(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/))
-    })
+  it('should update the model', (done) => {
+    const newText = 'abracadabra'
+    let originalUUID
+
+    textbox.sendKeys(expectedText)
+    selectDropdownByNumber(selectbox, 1, 100)
+      .then(() => browser.waitForAngular())
+      .then(() => saveDraftButton.click())
+      .then(() => browser.waitForAngular())
+      .then(() => lastSaved.getText().then((uuid) => (originalUUID = uuid)))
+      .then(() => textbox.clear())
+      .then(() => browser.waitForAngular())
+      .then(() => browser.sleep(1000))
+      .then(() => textbox.sendKeys(newText))
+      .then(() => browser.waitForAngular())
+      .then(() => saveDraftButton.click())
+      .then(() => browser.waitForAngular())
+      .then(() => {
+        const options = updatedSelectbox.all(by.tagName('option'))
+
+        expect(lastSaved.getText()).not.toBeUndefined()
+        expect(lastSaved.getText()).toBe(originalUUID)
+        expect(lastSaved.getText()).toEqual(jasmine.stringMatching(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/))
+        EC.textToBePresentInElementValue(updatedTextbox, newText)
+        expect(options.count()).toBe(2)
+        expect(options.first().isSelected()).toBe(true)
+        expect(updatedDateCreated).not.toBeUndefined()
+        EC.textToBePresentInElementValue(updatedFormName, expectedFormName)
+      })
+      .then(() => updatedDateCreated.getAttribute('value').then((created) => {
+        return updatedDateModified.getAttribute('value').then((updated) => {
+          expect((new Date(updated).getTime()) > (new Date(created)).getTime()).toBe(true)
+        })
+      }))
+      .then(done)
   })
 })
