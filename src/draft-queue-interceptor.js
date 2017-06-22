dd3891e82820de4d80c76de3b9f6cf3fd462a1bb
simonmarklar <simondobie23@gmail.com>
2017-06-22 13:55:32 +1000

    Add HTTP interceptor and unit tests

---
+++ src/draft-queue-interceptor.js
@@ -0,0 +1,23 @@
+'use strict'
+
+draftQueueInteceptor.$inject = ['$window', '$q', 'bmDraftQueueService']
+function draftQueueInteceptor($window, $q, bmDraftQueueService) {
+  const isPOSTorPUT = method => ['POST', 'PUT'].indexOf(method) > -1
+  const isFormData = (contentType) => contentType.toLowerCase().indexOf('application/x-www-form-urlencoded') > -1
+  const isJSONData = (contentType) => contentType.toLowerCase().indexOf('json') > -1
+
+  const isForm = (config) => isPOSTorPUT(config.method.toUpperCase()) &&
+                             (isFormData(config.headers['Content-Type']) || isJSONData(config.headers['Content-Type']))
+
+  return {
+    response: function DraftQueueResponse (response) {
+      if (!response.config || !isForm(response.config) || !response.config.data._uuid) {
+        return response
+      }
+
+      return bmDraftQueueService.removeItem(response.config.data._uuid).then(() => response)
+    },
+  }
+}
+
+module.exports = draftQueueInteceptor
